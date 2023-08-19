0577ba076056378f85e081f371e86c1bb759e070
Robbie Prokop <robbieprokop@gmail.com>
2023-08-18 18:57:01 -0600

    begin products

---
+++ frontend/app/components/Product/Product.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./product.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+
+interface ProductProps {
+  product: {
+    _id: string;
+    name: string;
+    image: string;
+    description: string;
+    brand: string;
+    category: string;
+    price: number;
+    countInStock: number;
+    rating: number;
+    numReviews: number;
+  };
+}
+
+const Product = ({ product }: ProductProps) => {
+  return (
+    <article className={styles.card}>
+      <Link href={`/product/${product._id}`}>
+        <Image src={product.image} alt="Product Image" />
+      </Link>
+      <div className={styles.cardBody}>
+        <Link href={`/product/${product._id}`}>
+          <h2>
+            <strong>{product.name}</strong>
+          </h2>
+        </Link>
+        <h3>${product.price}</h3>
+      </div>
+    </article>
+  );
+};
+export default Product;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import products from "./products";
+import Product from "./components/Product/Product";
 
 const Home = () => {
   return (
     <main>
       <div className="container">
         <h1>Zizi's Teashop</h1>
+
+        <div>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
       </div>
     </main>
   );
