d17c97fb23d7f5a39ec3097579eecc6b5ae78f95
Robbie Prokop <robbieprokop@gmail.com>
2023-08-21 15:22:00 -0600

    get nodemon installed

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import products from "./data/products.ts";
+import products from "./data/products.js";
 const PORT = 8080;
 
 const app = express();
 
 app.get("/", (req, res) => {
   console.log("Hello From Server");
+  res.json({ message: "Hello From Server" });
 });
 
 app.get("/api/products", (req, res) => {
+  console.log("products");
   res.json(products);
 });
 
