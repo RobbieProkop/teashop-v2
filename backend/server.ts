0322fe21b3c842a94d68f99e13fc51016c9dc7d7
Robbie Prokop <robbieprokop@gmail.com>
2023-08-21 14:54:00 -0600

    getting tsconfig setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import products from "./data/products.ts";
 const PORT = 8080;
 
 const app = express();
 
 app.get("/", (req, res) => {
   console.log("Hello From Server");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
