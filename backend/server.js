bf1a372a6bfaa90f7be02a34032d9220f6d54379
Robbie Prokop <robbieprokop@gmail.com>
2023-08-21 13:15:31 -0600

    start working on backend express server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const PORT = 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Hello From Server");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
