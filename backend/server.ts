8aec0575fc033f942036802e559194b6f1079b3e
Robbie Prokop <robbieprokop@gmail.com>
2023-08-24 11:16:12 -0600

    create ProductType

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import { ProductType } from "./types.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -16,7 +17,7 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
+  const product = products.find((p: ProductType) => p._id === req.params.id);
   res.json(product);
 });
 
