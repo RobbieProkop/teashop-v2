909a467880edd3d0f3e6dce4b8eebb5986b5b141
Robbie Prokop <robbieprokop@gmail.com>
2023-09-05 13:00:15 -0600

    working on seeding

---
+++ backend/seeder.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Order from "./models/orderModel.js";
+import Product from "./models/productModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const importData = async () => {
+  try {
+    //first delete all products and users before importing
+    await Order.deleteMany();
+    await User.deleteMany();
+    await Product.deleteMany();
+
+    //insert users
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log("Data Imported!".green.inverse);
+
+    process.exit();
+  } catch (error) {
+    console.log("error", error.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+  } catch (error) {
+    console.log("error", error);
+    process.exit(1);
+  }
+};
