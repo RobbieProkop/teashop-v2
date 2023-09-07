f119ab4598f29d8d943159a546bd4e87f00e92a2
Robbie Prokop <robbieprokop@gmail.com>
2023-09-07 15:51:19 -0600

    clean up routes

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { ProductType } from "../types.js";
 import {
   getProductById,
   getProducts,
 } from "../controllers/productController.js";
 const router = express.Router();
 
-router.get("/", getProducts);
-router.get("/:id", getProductById);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
