e3e7d95ca428fdee4ba6ffc634c6f38e1499a6ad
Robbie Prokop <robbieprokop@gmail.com>
2023-09-07 13:36:55 -0600

    get seeder working

--- backend/seeder.ts
+++ backend/seeder.ts
@@ -7,45 +7,43 @@ import User from "./models/userModel.js";
 import Order from "./models/orderModel.js";
 import Product from "./models/productModel.js";
 import connectDB from "./config/db.js";
-
 dotenv.config();
 connectDB();
-
 const importData = async () => {
   try {
     //first delete all products and users before importing
     await Order.deleteMany();
     await User.deleteMany();
     await Product.deleteMany();
-
     //insert users
     const createdUsers = await User.insertMany(users);
-
     const adminUser = createdUsers[0]._id;
     const sampleProducts = products.map((product) => {
       return { ...product, user: adminUser };
     });
-
     await Product.insertMany(sampleProducts);
-
     console.log("Data Imported!".green.inverse);
-
     process.exit();
   } catch (error) {
     console.log("error", error.red.inverse);
     process.exit(1);
   }
 };
-
 const destroyData = async () => {
   try {
     await Order.deleteMany();
     await Product.deleteMany();
     await User.deleteMany();
-
     console.log("Data Destroyed".red.inverse);
   } catch (error) {
     console.log("error", error);
     process.exit(1);
   }
 };
+if (process.argv[2] === "-d") {
+  // return destroyData()
+  console.log("deletedddddde");
+} else {
+  // importData()
+  console.log("importedddd");
+}
