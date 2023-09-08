7231856102bc56911b7c2e488cff607a5e70d254
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 10:25:23 -0600

    thinking abpout state

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 
@@ -9,6 +10,12 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 //Routes
 app.get("/", (req, res) => {
--- frontend/app/actions/actions.ts
+++ frontend/app/actions/actions.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
 
-export const getProducts = async () => {
-  const { data } = await axios.get("http://localhost:8080/api/products");
-  if (!data) throw new Error("Error in fetching products");
-  return data;
+export const getProducts = () => {
+  axios
+    .get("/api/products")
+    .then((data) => {
+      if (!data) throw new Error("Error in fetching products");
+      return data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const getSingleProduct = async (id: string) => {
-  const { data } = await axios.get(`http://localhost:8080/api/products/${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
   if (!data) throw new Error("Error in fetching product");
   return data;
 };
