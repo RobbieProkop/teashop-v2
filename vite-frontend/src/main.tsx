79700046b6b19a8650be595a4499bdc292c12b74
Robbie Prokop <robbieprokop@gmail.com>
2023-09-11 18:35:18 -0600

    install redux toolkit

--- vite-frontend/src/main.tsx
+++ vite-frontend/src/main.tsx
@@ -10,6 +10,8 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +24,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
